90698d91ffdd3ff8cd81709cf7c9413ea00f1079
wudu <wd@scsvision.com>
2018-12-27 17:38:00 +0800

    增加登录模块，设计文章表的结构

--- serves/connectDB.js
+++ serves/connectDB.js
@@ -16,9 +16,18 @@ const Schema = mongoose.Schema
 // 定义用户表数据结构
 const userModel = new Schema({
   id: String,
-  nickNam: String,
+  nickname: String,
   password: String
 })
-
+// 定义文章数据结构
+const articleModel = new Schema({
+  id: String,
+  title: String,
+  time: String, 
+  skeleton: String, // 纲要
+  content: String, // 内容
+  comment: Array, // 评论
+})
 // 将表的数据结构和表关联起来
 exports.user = mongoose.model('users', userModel, 'users')
+exports.article = mongoose.model('articles', articleModel, 'articles')
--- serves/index.js
+++ serves/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser())
 router.get('/', async (ctx, next) => {
   ctx.response.body = `<h1>Hello World</h1>`
 })
+// 注册
 router.post('/register', async (ctx, next) => {
   let password = crypto.createHash('md5').update(ctx.request.body.password).digest('hex')
   await user.create({
@@ -21,7 +22,22 @@ router.post('/register', async (ctx, next) => {
   }).then(() => ctx.response.body = '注册成功')
   .catch(err => console.log(err))
 })
-
+// 登录
+router.post('/login', async (ctx, next) => {
+  let password = crypto.createHash('md5').update(ctx.request.body.password).digest('hex')
+  await user.find({'nickname': ctx.request.body.nickname, 'password': password}, function (err, data) {
+    if (err) throw err
+    if (!data.toString()) {
+      ctx.response.body = '用户名或密码错误'
+    } else {
+      ctx.response.body = '登录成功'
+    }
+  })
+})
+// 发布文章
+router.post('/pushArticle', async (ctx, next) => {
+  
+})
 // 添加路由中间件
 app.use(router.routes())
 
